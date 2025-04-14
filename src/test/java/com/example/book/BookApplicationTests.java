07fde958b6052c908c14ad1ac49357ac670030d8
sami7453 <145992280+sami7453@users.noreply.github.com>
2025-04-14 10:39:52 +0200

    add tests

--- src/test/java/com/example/book/BookApplicationTests.java
+++ src/test/java/com/example/book/BookApplicationTests.java
@@ -3,11 +3,32 @@ package com.example.book;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
-class BookApplicationTests {
+class BookTests {
 
 	@Test
-	void contextLoads() {
+	void testBookConstructorAndGetters() {
+		Book book = new Book(1, "1984", "George Orwell", 1949);
+
+		assertEquals(1, book.getId());
+		assertEquals("1984", book.getTitle());
+		assertEquals("George Orwell", book.getAuthor());
+		assertEquals(1949, book.getYear());
 	}
 
+	@Test
+	void testSetters() {
+		Book book = new Book();
+		book.setId(2);
+		book.setTitle("Sapiens");
+		book.setAuthor("Yuval Noah Harari");
+		book.setYear(2011);
+
+		assertEquals(2, book.getId());
+		assertEquals("Sapiens", book.getTitle());
+		assertEquals("Yuval Noah Harari", book.getAuthor());
+		assertEquals(2011, book.getYear());
+	}
 }
